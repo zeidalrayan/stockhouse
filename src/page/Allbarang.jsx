681b7bab426cb621417205a32f0b132ae714ffe2
zeidalrayan <zeidalrayan@gmail.com>
2024-10-01 11:01:10 +0700

    1.1

--- src/page/Allbarang.jsx
+++ src/page/Allbarang.jsx
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from "react";
-import Layout from "../page/Layout";
-import { supabase } from "../utils/SupaClient";
-import { Spinner } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Spinner,
+} from "@nextui-org/react";
 import useFormatRupiah from "../hooks/useformatrupiah";
 import useTrancate from "../hooks/useTrancate";
+import { supabase } from "../utils/SupaClient";
 
 const Allbarang = () => {
   const [allSupliers, setAllSupliers] = useState([]);
@@ -11,6 +19,8 @@ const Allbarang = () => {
   const [loading, setLoading] = useState(true);
   const { formatrupiah } = useFormatRupiah();
   const { trunscatetext } = useTrancate();
+  const [selectedItem, setSelectedItem] = useState(null); // State for selected item
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const getAllData = async () => {
     setLoading(true);
@@ -33,24 +43,73 @@ const Allbarang = () => {
     }
   };
 
+  const handleCardClick = (item) => {
+    setSelectedItem(item);
+    onOpen();
+  };
+
   useEffect(() => {
     getAllData();
-    document.getElementById("title").innerHTML =
-      "Table all barang and supliers";
   }, []);
 
   return (
-    <Layout>
+    <section id="dataTables" className="p-8">
       {loading ? (
         <Spinner className="flex flex-col justify-center h-full" />
       ) : (
-        <section id="dataTables" className="p-8">
+        <>
+          {/* Modal Component */}
+          <Modal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            isDismissable={false}
+          >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">
+                    {selectedItem?.nama_supplier || selectedItem?.nama_barang}
+                  </ModalHeader>
+                  <ModalBody>
+                    {selectedItem?.logo_supplier && (
+                      <img
+                        src={selectedItem.logo_supplier}
+                        alt={selectedItem.nama_supplier}
+                        className="w-full h-32 object-contain"
+                      ></img>
+                    )}
+                    {selectedItem?.alamat && (
+                      <p>Alamat: {selectedItem.alamat}</p>
+                    )}
+                    {selectedItem?.email && <p>Email: {selectedItem.email}</p>}
+                    {selectedItem?.harga && (
+                      <p>Harga: {formatrupiah(selectedItem.harga)}</p>
+                    )}
+                    {selectedItem?.stok && <p>Stok: {selectedItem.stok}</p>}
+                    {selectedItem?.deskripsi && (
+                      <p>Deskripsi: {selectedItem.deskripsi}</p>
+                    )}
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="light" onPress={onClose}>
+                      Close
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
+
           <div className="mb-10">
             <h2 className="text-2xl font-bold mb-4">Suplier Data</h2>
             <div className="grid grid-cols-2 gap-8 max-lg:grid-cols-1">
               {allSupliers.length > 0 ? (
                 allSupliers.map((suplier, index) => (
-                  <div key={index} className="mb-6 p-4 border rounded-lg">
+                  <div
+                    key={index}
+                    className="mb-6 p-4 border rounded-lg cursor-pointer"
+                    onClick={() => handleCardClick(suplier)}
+                  >
                     {suplier.logo_supplier && (
                       <img
                         src={suplier.logo_supplier}
@@ -78,7 +137,8 @@ const Allbarang = () => {
                 allBarang.map((barang, index) => (
                   <div
                     key={index}
-                    className="mb-6 p-4 border border-gray-400 rounded-lg"
+                    className="mb-6 p-4 border rounded-lg cursor-pointer"
+                    onClick={() => handleCardClick(barang)}
                   >
                     {barang.foto_barang && (
                       <img
@@ -92,7 +152,17 @@ const Allbarang = () => {
                     </h3>
                     <p>Harga: {formatrupiah(barang.harga)}</p>
                     <p>Stok: {barang.stok}</p>
-                    <p>Deskripsi: {trunscatetext(barang.deskripsi, 100)}</p>
+                    <p>Deskripsi: {trunscatetext(barang.deskripsi, 50)}</p>
+                    {allSupliers.length > 0 && (
+                      <p>
+                        Suplier:{" "}
+                        {
+                          allSupliers.find(
+                            (suplier) => suplier.id === barang.suplier_id
+                          )?.nama_supplier
+                        }
+                      </p>
+                    )}
                   </div>
                 ))
               ) : (
@@ -100,9 +170,9 @@ const Allbarang = () => {
               )}
             </div>
           </div>
-        </section>
+        </>
       )}
-    </Layout>
+    </section>
   );
 };
 
